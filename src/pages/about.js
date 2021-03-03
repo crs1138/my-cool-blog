3d79de7c7983c5e0d47a84fcef8d7054b62e4a1f
Jan Pozivil <crs1138@me.com>
2021-03-03 19:12:40 +0100

    🌟 Add About page
    Details:
    ===
    
    * add About page
    * update front page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+const AboutPage = () => {
+    return (
+        <main>
+            <h1>About me</h1>
+            <p>I'm building a Gatsby blog</p>
+        </main>
+    );
+};
+
+export default AboutPage;
